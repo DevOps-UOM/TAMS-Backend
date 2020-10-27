78866418d30efde78d19f3238e84e3bd858a7872
JohanPerera <johanpriyasanka@gmail.com>
2020-10-27 20:53:12 +0530

    Itinerary component is done

---
+++ api/routes/ItineraryRoute.js
@@ -0,0 +1,19 @@
+module.exports = function(app) {
+    var itineraries = require('../controllers/ItineraryController');
+
+    //get all
+    app.route('/itineraries').get(itineraries.listAllItineraries);
+
+    //create new
+    app.route('/itineraries').post(itineraries.addAItinerary);
+
+    //get single Itinerary
+    app.route('/itineraries/:date/:taid').get(itineraries.getASingleItinerary);
+
+    //edit Itinerary
+    app.route('/itinerarie/:date/:taid').put(itineraries.updateAItinerary);
+
+    //delete Itinerary
+    app.route('/itineraries/:date/:taid').delete(itineraries.deleteAItinerary);
+
+}
---
+++ app.js
@@ -0,0 +1,47 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+const cors = require('cors');
+const uri = "mongodb+srv://dbAdmin:LGPxREeweWiVjnPM@clustertams.ovlfe.mongodb.net/TRAVELING-AGENTS-MANAGEMENT-SYSTEM?retryWrites=true&w=majority";
+
+var app = express();
+
+app.use(cors());
+
+var port = process.env.PORT || 3000;
+
+var Itinerary = require('./api/models/ItineraryModel');
+
+mongoose.Promise = global.Promise;
+
+const connectDB = async() => {
+    await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("db connected");
+}
+connectDB();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routeItinerary = require('./api/routes/ItineraryRoute');
+
+routeItinerary(app);
+
+app.use(function(req, res) {
+    res.status(404).send({ url: req.originalUrl + ' not found!' });
+});
+
+app.listen(port);
+
+console.log("TAMS Restful API server started on :" + port);
+
+/*
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://dbAdmin:LGPxREeweWiVjnPM@clustertams.ovlfe.mongodb.net/TRAVELING-AGENTS-MANAGEMENT-SYSTEM?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
