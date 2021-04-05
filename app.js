46200004ce55399061b7dbb897b06ea47c2bea2a
JohanPerera <johanpriyasanka@gmail.com>
2021-04-05 23:38:39 +0530

    deploy test

--- app.js
+++ app.js
@@ -34,15 +34,15 @@ connectDB();
 
 
 
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
 
-app.use(requireHTTPS);
+// app.use(requireHTTPS);
 
 
 
