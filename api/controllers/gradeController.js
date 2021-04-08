ea77ce2f0417baadea526bdd6f4e4e91de382c5f
JohanPerera <johanpriyasanka@gmail.com>
2021-04-09 00:29:14 +0530

    deploy test

--- api/controllers/gradeController.js
+++ api/controllers/gradeController.js
@@ -1,24 +1,24 @@
-const ResponseService = require("../shared/ResponseService");
+const ResponseService = require("../shared/responseService");
 const mongoose = require('mongoose');
 Grade = mongoose.model('Grade');
 
 exports.createUser = function(req, res) {
-  const grade = new Grade(req.body);
-  grade.save((err, doc) => {
-    ResponseService.generalPayloadResponse(err, doc, res);
-  });
+    const grade = new Grade(req.body);
+    grade.save((err, doc) => {
+        ResponseService.generalPayloadResponse(err, doc, res);
+    });
 };
 
 exports.getUser = function(req, res) {
-  Grade.find({ agentType: "ca" }, (err, doc) => {
-    ResponseService.generalPayloadResponse(err, doc, res);
-  });
+    Grade.find({ agentType: "ca" }, (err, doc) => {
+        ResponseService.generalPayloadResponse(err, doc, res);
+    });
 };
 
 exports.getMaxUser = function(req, res) {
-  Grade.find({ agentType: "ta" }, (err, doc) => {
-    ResponseService.generalPayloadResponse(err, doc, res);
-  });
+    Grade.find({ agentType: "ta" }, (err, doc) => {
+        ResponseService.generalPayloadResponse(err, doc, res);
+    });
 };
 
 
